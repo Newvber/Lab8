20beaf1f920322d194dcbe0fa240278b0f8b9911
Newvber <weibo2@ualberta.ca>
2024-10-25 17:09:55 -0600

    hasCity success

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 public class CustomListTest {
     public CustomList MockCityList() {
         CustomList list = new CustomList(null, new ArrayList<>());
-        cityList.add(mockCity());
+        list.add(mockCity());
         return list;
     }
 
