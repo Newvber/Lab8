ec511a3e1ee772ca5bcda522270fb674c02d19fc
Newvber <weibo2@ualberta.ca>
2024-10-25 16:30:40 -0600

    update

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -50,7 +50,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void addCity(City city){
-        cities.add(city);
+//        cities.add(city);
     }
 
     public void deleteCity(City city) {
